68fb0f5e630aafbf81ea4624ce6a4e6b67656912
Bryan Aguilar <bryan.aguilar6174@gmail.com>
2025-02-09 17:06:50 -0500

    🧹 chore: Remove freezed dependencies

--- lib/features/profile/presentation/cubit/profile_cubit.dart
+++ lib/features/profile/presentation/cubit/profile_cubit.dart
@@ -1,9 +1,8 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'profile_state.dart';
-part 'profile_cubit.freezed.dart';
 
 class ProfileCubit extends Cubit<ProfileState> {
   ProfileCubit() : super(ProfileState(lang: 'en'));
--- lib/features/profile/presentation/cubit/profile_state.dart
+++ lib/features/profile/presentation/cubit/profile_state.dart
@@ -7,13 +7,62 @@ enum ActiveTheme {
 
   final ThemeMode mode;
   const ActiveTheme(this.mode);
+
+  // Convert enum to JSON
+  String toJson() => name;
+
+  // Convert JSON to enum
+  static ActiveTheme fromJson(String json) {
+    return ActiveTheme.values.firstWhere(
+      (theme) => theme.name == json,
+      orElse: () => ActiveTheme.system, // Default value if not found
+    );
+  }
 }
 
-@freezed
-class ProfileState with _$ProfileState {
-  const factory ProfileState({
+class ProfileState extends Equatable {
+  final String? title;
+  final String? lang;
+  final ActiveTheme activeTheme;
+
+  const ProfileState({
+    this.title,
+    this.lang,
+    this.activeTheme = ActiveTheme.system,
+  });
+
+  ProfileState copyWith({
     String? title,
     String? lang,
-    @Default(ActiveTheme.light) ActiveTheme activeTheme,
-  }) = _ProfileState;
+    ActiveTheme? activeTheme,
+  }) {
+    return ProfileState(
+      title: title ?? this.title,
+      lang: lang ?? this.lang,
+      activeTheme: activeTheme ?? this.activeTheme,
+    );
+  }
+
+  factory ProfileState.fromJson(Map<String, dynamic> json) {
+    return ProfileState(
+      title: json['title'] as String?,
+      lang: json['lang'] as String?,
+      activeTheme: ActiveTheme.fromJson(json['activeTheme']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'lang': lang,
+      'activeTheme': activeTheme.toJson(),
+    };
+  }
+
+  @override
+  List<Object?> get props => [title, lang, activeTheme];
+
+  @override
+  String toString() =>
+      'ProfileState(title: $title, lang: $lang, activeTheme: $activeTheme)';
 }
